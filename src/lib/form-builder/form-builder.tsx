71feb347936c29cdd1abba99a9eb71ffd7f9c3fd
EldarTarieladze <68877428+EldarTarieladze@users.noreply.github.com>
2021-07-30 07:29:36 -0700

    textField
    textField

--- src/lib/form-builder/form-builder.tsx
+++ src/lib/form-builder/form-builder.tsx
@@ -1,5 +1,9 @@
-import { Box, Paper, Typography } from '@material-ui/core'
+import { Box, Paper, Typography, TextField, Button } from '@material-ui/core'
+import { log } from 'console'
+import { StringDecoder } from 'node:string_decoder'
+import { getConstantValue } from 'typescript'
 import { ObjectSchema } from './types'
+import React, {useEffect, useState} from "react"
 
 type Props = {
   schema: ObjectSchema
@@ -7,6 +11,20 @@ type Props = {
 }
 
 export const FormBuilder = ({ schema, onSubmit }: Props) => {
+  const [data, setData] = useState("");
+  const func = (value: string, property: string): void =>{
+      setData(value)
+      console.log(value, property)
+  }
+  const onsub = (): void =>{
+    console.log(data)
+
+  }
+
+  const App = () => {
+  };
+
+
   return (
     <Paper>
       <Box
@@ -15,13 +33,42 @@ export const FormBuilder = ({ schema, onSubmit }: Props) => {
         flexDirection="column"
         component="form"
         onSubmit={event => {
-          // Code here
+          onsub()
         }}
       >
         <Typography variant="h5" gutterBottom>
           {schema.label}
         </Typography>
+        {
+          schema.properties.map((item) => {
+            return(
+              <>
+                <TextField
+                name={item.name}
+                type={item.type}
+                id="outlined-basic"
+                label={item.label}
+                variant="outlined"
+                onChange={(e) =>{
+                    func(e.target.value, e.target.name)
+                }}
+              >
+            </TextField>
+              </>
+            )
+          })
+        }
         {/* Code here */}
+        <Button
+        variant="contained"
+        color="primary"
+        onClick={() =>{
+          onsub()
+        }}
+
+        >
+          Submitt
+        </Button>
       </Box>
     </Paper>
   )
